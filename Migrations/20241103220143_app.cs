a2a97be65ba499244fbf85cd8ffa9c7f962d41c4
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-11-03 18:06:35 -0400

    Azure

---
+++ Migrations/20241103220143_app.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharkStyleApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class app : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Imagenn",
+                table: "Categorias",
+                newName: "Imagen");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Imagen",
+                table: "Categorias",
+                newName: "Imagenn");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,11 +19,9 @@ namespace SharkStyleApi
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
 
             app.UseHttpsRedirection();
 
