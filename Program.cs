721d4747d85001c180226646100256f725529157
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2026-04-10 00:08:37 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using SharkStyleApi.DAL;
 
-namespace SharkStyleApi
+namespace SharkStyleApi;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            var ConStr = builder.Configuration.GetConnectionString("ConStr");
-            builder.Services.AddDbContext<Contexto>(options =>
-                options.UseSqlServer(ConStr));
+        var ConStr = builder.Configuration.GetConnectionString("ConStr");
+        builder.Services.AddDbContext<Contexto>(options =>
+            options.UseNpgsql(ConStr));
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        
 
-            app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+        app.UseAuthorization();
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.Run();
-        }
+        app.Run();
     }
 }
