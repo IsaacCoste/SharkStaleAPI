9a0f364bb5276e4af3f72d5defd02cc7105a8519
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2026-04-10 00:08:39 -0400

    Update SharkStyleApi.data/

---
+++ SharkStyleApi.data/Models/Carrito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkStyleApi.data.Models;
+public class Carrito
+{
+    public int CarritoId { get; set; }
+    public int UsuarioId { get; set; }
+    public bool Pagado { get; set; } = false;
+    public DateTime FechaCreacion { get; set; } = DateTime.Now;
+    public Usuario Usuario { get; set; }
+
+    public ICollection<DetalleCarrito> Detalles { get; set; } = new List<DetalleCarrito>();
+}
---
+++ SharkStyleApi.data/Models/DetalleCompra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharkStyleApi.data.Models;
+public class DetalleCompra
+{
+    public int DetalleCompraId { get; set; }
+    public int CompraId { get; set; }
+    public int DetalleProductoId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+    public Compra Compra { get; set; }
+    public DetalleProducto DetalleProducto { get; set; }
+}
